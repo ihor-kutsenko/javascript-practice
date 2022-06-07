f44cd1587355660ab8f95a3b760e25008f3cd366
Rammbest <96702664+ihor-kutsenko@users.noreply.github.com>
2022-06-08 00:22:46 +0300

    array3

--- js/array/backnumbers.js
+++ js/array/backnumbers.js
@@ -65,3 +65,38 @@ console.log(filterArray([1, 2, 3, 4, 5], 4));
 console.log(filterArray([1, 2, 3, 4, 5], 5));
 console.log(filterArray([12, 24, 8, 41, 76], 38));
 console.log(filterArray([12, 24, 8, 41, 76], 20));
+
+//
+//
+function filterArray(numbers, value) {
+  const filteredNumbers = [];
+  for (const number of numbers) {
+    if (number > value) {
+      filteredNumbers.push(number);
+    }
+  }
+  return filteredNumbers;
+}
+//
+//
+// Напиши функцію getEvenNumbers(start, end), яка повертає масив
+// усіх парних чисел від start до end.Парним вважається число, яке
+// ділиться на 2 без остачі(10 % 2 === 0).
+
+// Оголошена функція getEvenNumbers(start, end)
+// Виклик функції getEvenNumbers(2, 5) повертає [2, 4]
+// Виклик функції getEvenNumbers(3, 11) повертає [4, 6, 8, 10]
+// Виклик функції getEvenNumbers(6, 12) повертає [6, 8, 10, 12]
+// Виклик функції getEvenNumbers(8, 8) повертає [8]
+// Виклик функції getEvenNumbers(7, 7) повертає []
+// Виклик функції getEvenNumbers() з випадковими start і end повертає правильний масив
+function getEvenNumbers(start, end) {
+  let numberBack = [];
+  for (let i = start; i <= end; i += 1) {
+    if (i % 2 === 0) {
+      numberBack.push(i);
+    }
+  }
+
+  return numberBack;
+}
--- js/function-calculateTotalPrice.js
+++ js/function-calculateTotalPrice.js
@@ -46,3 +46,14 @@ function calculateTotalPrice(order) {
 console.log(calculateTotalPrice([12, 85, 37, 4]));
 console.log(calculateTotalPrice([164, 48, 291]));
 console.log(calculateTotalPrice([412, 371, 94, 63, 176]));
+
+//
+function calculateTotalPrice(order) {
+  let total = 0;
+
+  for (const x of order) {
+    total += x;
+  }
+
+  return total;
+}

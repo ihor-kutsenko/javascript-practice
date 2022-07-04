6bec872b2d9ad396e07b1340a0f31c39d95089b8
Rammbest <96702664+ihor-kutsenko@users.noreply.github.com>
2022-07-04 22:29:08 +0300

    function

---
+++ js/a.js
@@ -0,0 +1,10 @@
+function calculateTotal(number) {
+  // Change code below this line
+  let total = 0;
+  for (let i = 0; i <= number; i += 1) {
+    total += i;
+  }
+  return total;
+
+  // Change code above this line
+}
